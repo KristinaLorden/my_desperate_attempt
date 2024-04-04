b84367fd5122818b96a6b67223dd322b7ea62e8f
KristinaLorden <104163735+KristinaLorden@users.noreply.github.com>
2024-04-04 22:47:20 +0300

    Add files via upload

---
+++ my_desperate_attempt.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "log"
+)
+
+func main() {
+  var a string
+
+  for {
+    fmt.Println("Do you still love me?")
+    _, err := fmt.Scan(&a)
+    if err != nil {
+      log.Fatal("Все сломалось")
+    }
+    if a == "Yes" {
+      fmt.Println("I love you too... I'm very sorry about what happened, and I want to talk to you tet-a-tet. Please, text me about your thoughts on telegram")
+      break
+    } else {
+      fmt.Println("I understand you. Please let me thank you for everything that happened between us. And then I'll never bother you.")
+    }
+  }
+
+}
